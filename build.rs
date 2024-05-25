8a89ad7b96f264f6bc4fa24baf730f2058a9c102
Sidney Bovet <sidney.bovet@gmail.com>
2024-05-25 13:48:53 +0200

    Add basic proto support, already with pbjson

---
+++ build.rs
@@ -0,0 +1,31 @@
+use std::{env, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("proto");
+    let proto_files = vec![root.join("config.proto")];
+
+    // Tell cargo to recompile if any of these proto files are changed
+    for proto_file in &proto_files {
+        println!("cargo:rerun-if-changed={}", proto_file.display());
+    }
+
+    // Prost-build the protos, with descriptor files
+    let descriptor_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("proto_descriptor.bin");
+    prost_build::Config::new()
+        // Save descriptors to file
+        .file_descriptor_set_path(&descriptor_path)
+        // Override prost-types with pbjson-types
+        .compile_well_known_types()
+        .extern_path(".google.protobuf", "::pbjson_types")
+        // Generate prost structs
+        .compile_protos(&proto_files, &[root])?;
+
+    // Read descriptor for JSON parsing
+    let descriptor_set = std::fs::read(descriptor_path)?;
+    pbjson_build::Builder::new()
+        .register_descriptors(&descriptor_set)?
+        // This is the proto package name
+        .build(&[".api_config"])?;
+
+    Ok(())
+}
